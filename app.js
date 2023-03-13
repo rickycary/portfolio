9b4532c70a4fe31789fdf26d06085b39119ccc43
Richard <ricky.cary1@gmail.com>
2023-03-13 16:00:43 -0500

    portfolio: Added JS to display about me sections

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ function opentab(tabName){
     for(tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");
     }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+
+
 }
